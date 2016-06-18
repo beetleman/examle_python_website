131a3d6b4414bad7cf598a0a8a2d99b72bb619a5
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-06-18 23:24:35 +0200

    add tests for `todo` view

---
+++ src/tests/test_todo.py
@@ -0,0 +1,13 @@
+def test_todo_get(client):
+    response = client.get('/todo')
+    assert response.status_code == 200
+    assert 'TODO: ' in response.data.decode()
+
+
+def test_todo_post(client):
+    new_item = 'noooopeee'
+    response = client.post('/todo',  data=dict(
+        item=new_item,
+    ), follow_redirects=True)
+    assert response.status_code == 200
+    assert '<li>{}</li>'.format(new_item) in response.data.decode()
