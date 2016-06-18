34af80afe8d002002cea77601a879ce8a0cf37f8
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-06-19 00:47:08 +0200

    all tests passed app working!

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -6,9 +6,9 @@ def test_create_and_get_item(app):
     text = 'Yoooloooo'
     item = TodoItem(text=text)
     item.save()
-    item_fetched = TodoItem.get(
+    item_fetched = TodoItem.select().where(
         TodoItem.id==item.id
-    )
+    ).get()
 
     assert item.id is not None
     assert item.id == item_fetched.id
--- src/views.py
+++ src/views.py
@@ -1,4 +1,9 @@
-from flask import render_template, Blueprint
+# -*- coding: utf-8 -*-
+from flask import render_template, Blueprint, request
+from flask.views import MethodView
+
+from models import TodoItem
+
 
 simple_page = Blueprint(
     'simple_page', __name__,
@@ -9,3 +14,22 @@ simple_page = Blueprint(
 @simple_page.route('/')
 def index():
     return render_template('index.html')
+
+
+class ToDoListView(MethodView):
+
+    def render_todo(self):
+        items = TodoItem.select()
+        return render_template('todo.html', items=items)
+
+    def get(self):
+        return self.render_todo()
+
+    def post(self):
+        text = request.form.get('text')
+        item = TodoItem(text=text)
+        item.save()
+        return self.render_todo()
+
+
+simple_page.add_url_rule('/todo/', view_func=ToDoListView.as_view('todo'))
